d2b539029b92df857fe3e9efba509c25488a3a9a
quarxz <36408691+quarxz@users.noreply.github.com>
2024-03-28 14:17:13 +0100

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+'use client'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center space-y-8 bg-gray-900 p-24">
+      <h1>The Notebook for a chuckle</h1>
+      {/* Jokes */}
+      {/* Form */}
+      {/* Preview */}
+    </main>
+  )
+}
